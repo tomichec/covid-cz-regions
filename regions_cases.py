4d2d3162817b7fcf24c78b70a9628932e10af199
TS <tomiche@gmail.com>
2020-03-29 18:27:37 +0200

    new format of the website, split the script into functions

--- regions_cases.py
+++ regions_cases.py
@@ -3,6 +3,10 @@ from bs4 import BeautifulSoup
 import json
 
 def main():
+    data = get_data()
+    print_data(data)
+
+def get_data():
     url = "https://onemocneni-aktualne.mzcr.cz/covid-19"
     response = requests.get(url)
     if (response.status_code != 200):
@@ -10,12 +14,12 @@ def main():
         return
 
     parsed_html = BeautifulSoup(response.text, "html.parser")
+    total_regions_data = parsed_html.body.find('div', attrs={'id':'js-total-isin-regions-data'})
 
-    total_regions_data = parsed_html.body.find('div', attrs={'id':'js-total-regions-data'})
-    data = json.loads(total_regions_data['data-barchart'])
+    return json.loads(total_regions_data['data-barchart'])
 
+def print_data(data):
     total = 0
-    
     print("{")
     for region in data['values']:
         print('\t"%s": %s,' % (region['x'],region['y']))
